c8067cdb85bc277ab8479a260c77b063814a8be5
David Garcia <david.garcia@revature.net>
2021-07-06 07:32:34 -0400

    removed user_post table from aws database and setup/teardown script

--- setup.sql
+++ setup.sql
@@ -8,28 +8,22 @@ create table users(
 
 create table post(
 	post_id serial primary key,
+	user_id integer references users,
 	title varchar(30),
 	country varchar(30),
 	city varchar(30),
 	tag varchar(30),
-	rating numeric constraint rating_less_than_five check (rating <= 5),
+	rating integer constraint rating_less_than_five check (rating <= 5),
 	constraint rating_greater_than_zero check (rating >= 0)
 );
 
-create table user_post (
-	surrogate_id serial primary key,
-	users_id integer references users,
-	post_id integer references post
-);
-
 -- DML
-insert into users values (default, 'alpha','password123');
 
+insert into users values (default, 'alpha','password123');
 insert into users values (default, 'beta','password123');
 insert into users values (default, 'charlie','password123');
-insert into post values (default,'Shrimp Linguini Alfredo','United States','New Orleans',null, 0);
-insert into post values (default, 'Cuban Sandwich', 'United States','Miami',null, 5);
-insert into post values (default, 'Vanilla Ice Cream', 'United States', 'Chicago',null, 3);
-insert into user_post values (default, 1, 1);
-insert into user_post values (default, 2, 2);
-insert into user_post values (default, 3, 3);
+
+
+insert into post values (default, 1, 'Shrimp Linguini Alfredo','United States','New Orleans',null,0);
+insert into post values (default, 2, 'Cuban Sandwich', 'United States','Miami',null,5);
+insert into post values (default, 3, 'Vanilla Ice Cream', 'United States','Chicago',null,3);
--- teardown.sql
+++ teardown.sql
@@ -1,3 +1,2 @@
-drop table user_post;
 drop table post;
 drop table users;
